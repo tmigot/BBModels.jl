dc0ca2131c7501391b7d8011e943eaad985c586d
tmigot <tangi.migot@gmail.com>
2022-12-21 16:47:40 -0500

    up `bb_meta`

--- src/meta.jl
+++ src/meta.jl
@@ -4,19 +4,18 @@ export BBModelMeta
 Struct containing the necessary information of black box optimization problem.
 Similar to `NLPModels.NLPModelMeta`, this struct keeps track of the types of variables (i.e, continuous, discrete and binary).
 """
-struct BBModelMeta{S}
+struct BBModelMeta
   nvar::Int
-  x0::S
-  x_n::Vector{String}
+  x_n::Vector{String} # do we need this?
   iint::Vector{Int}
   ifloat::Vector{Int}
   ibool::Vector{Int}
 
   function BBModelMeta(
     nvar::Int,
-    x0::S,
+    x0::AbstractVector,
     x_n::Vector{String}
-  ) where {S}
+  )
     if (nvar < 1)
       error("Nonsensical dimensions")
     end
@@ -28,6 +27,6 @@ struct BBModelMeta{S}
     ifloat = findall(x -> x isa AbstractFloat, x0)
     ibool = findall(x -> x isa Bool, x0)
 
-    new{S}(nvar, x0, x_n, iint, ifloat, ibool)
+    new(nvar, x_n, iint, ifloat, ibool)
   end
 end
