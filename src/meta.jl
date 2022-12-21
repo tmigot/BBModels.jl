31092e8ef5576a5de090b5cc26a80f9477f38005
tmigot <tangi.migot@gmail.com>
2022-12-21 16:33:49 -0500

    Clean `bb_meta`

--- src/meta.jl
+++ src/meta.jl
@@ -4,45 +4,30 @@ export BBModelMeta
 Struct containing the necessary information of black box optimization problem.
 Similar to `NLPModels.NLPModelMeta`, this struct keeps track of the types of variables (i.e, continuous, discrete and binary).
 """
-struct BBModelMeta
+struct BBModelMeta{S}
   nvar::Int
-  x0::Vector{Float64}
+  x0::S
   x_n::Vector{String}
-  lvar::Vector{Float64}
-  uvar::Vector{Float64}
   iint::Vector{Int}
   ifloat::Vector{Int}
   ibool::Vector{Int}
 
-  ncon::Int
-  lcon::Vector{Float64}
-  ucon::Vector{Float64}
-
   function BBModelMeta(
     nvar::Int,
-    x0::AbstractVector,
-    x_n::Vector{String},
-    lvar::AbstractVector,
-    uvar::AbstractVector;
-    ncon = 0,
-    lcon::Vector{Float64} = fill!(Vector{Float64}(undef, ncon), -Inf64),
-    ucon::Vector{Float64} = fill!(Vector{Float64}(undef, ncon), Inf64),
-  )
-    if (nvar < 1) || (ncon < 0)
+    x0::S,
+    x_n::Vector{String}
+  ) where {S}
+    if (nvar < 1)
       error("Nonsensical dimensions")
     end
 
-    @lencheck nvar x0 lvar uvar
-    @lencheck ncon lcon ucon
+    @lencheck nvar x0 x_n
 
-    S = Float64
+    # debug this...
     iint = findall(x -> x isa Int, x0)
     ifloat = findall(x -> x isa AbstractFloat, x0)
     ibool = findall(x -> x isa Bool, x0)
-    x0 = Vector{S}([S(i) for i in x0])
-    lvar = Vector{S}([S(i) for i in lvar])
-    uvar = Vector{S}([S(i) for i in uvar])
 
-    new(nvar, x0, x_n, lvar, uvar, iint, ifloat, ibool, ncon, lcon, ucon)
+    new{S}(nvar, x0, x_n, iint, ifloat, ibool)
   end
 end
