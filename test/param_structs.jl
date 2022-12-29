a22550a391b462cafe4bd7ac1461444fba14d192
tmigot <tangi.migot@gmail.com>
2022-12-29 02:00:23 -0500

    Up test constructor

--- test/param_structs.jl
+++ test/param_structs.jl
@@ -1,4 +1,4 @@
-struct R2ParameterSet{T <: AbstractFloat, I <: Integer, B <: Bool} <: AbstractParameterSet  #TODO change it to include  StochasticRounding
+struct R2ParameterSet{T <: AbstractFloat, I <: Integer} <: AbstractParameterSet
   atol::Parameter{T, RealInterval{T}}
   rtol::Parameter{T, RealInterval{T}}
   η1::Parameter{T, RealInterval{T}}
@@ -8,26 +8,26 @@ struct R2ParameterSet{T <: AbstractFloat, I <: Integer, B <: Bool} <: AbstractPa
   σmin::Parameter{T, RealInterval{T}}
   β::Parameter{T, RealInterval{T}}
   mem::Parameter{I, IntegerRange{I}}
-  is_scaling::Parameter{B, BinaryRange{B}}
+  is_scaling::Parameter{Bool, BinaryRange{Bool}}
 
-  function R2ParameterSet(
-    atol::T,
-    rtol::T,
-    η1::T,
-    η2::T,
-    γ1::T,
-    γ2::T,
-    σmin::T,
-    β::T,
-    mem::I,
-    is_scaling::B,
-  ) where {T <: AbstractFloat, I <: Integer, B <: Bool}
+  function R2ParameterSet{T, I}(;
+    atol::T = √eps(T),
+    rtol::T = √eps(T),
+    η1::T = T(eps(T)^(1 / 4)),
+    η2::T = T(0.95),
+    γ1::T = T(1 / 2),
+    γ2::T = T(2),
+    σmin::T = zero(T),
+    β::T = zero(T),
+    mem::I = I(5),
+    is_scaling::Bool = true,
+  ) where {T <: AbstractFloat, I <: Integer}
     (atol > 0) || error("invalid atol")
     (rtol > 0) || error("invalid rtol")
     (0 ≤ β < 1) || error("invalid: β needs to be between [0,1)")
     (γ1 ≤ γ2) || error("invalid γ1 <= γ2")
     (0 < η1 ≤ η2 ≤ 1) || error("invalid: 0 < η1 <= η2 <= 1")
-    p_set = new{T, I, B}(
+    p_set = new{T, I}(
       Parameter(T(atol), RealInterval(T(0), T(1)), ""),
       Parameter(T(rtol), RealInterval(T(0), T(1)), ""),
       Parameter(T(η1), RealInterval(T(0), T(1)), ""),
@@ -37,24 +37,13 @@ struct R2ParameterSet{T <: AbstractFloat, I <: Integer, B <: Bool} <: AbstractPa
       Parameter(σmin, RealInterval(T(0), T(1000)), ""),
       Parameter(T(β), RealInterval(T(0), T(1000)), ""),
       Parameter(I(mem), IntegerRange(I(5), I(20)), ""),
-      Parameter(B(is_scaling), BinaryRange(), ""),
+      Parameter(is_scaling, BinaryRange(), ""),
     )
     set_names!(p_set)
     return p_set
   end
-end
 
-function R2ParameterSet{T, I, B}() where {T <: AbstractFloat, I <: Integer, B <: Bool}
-  return R2ParameterSet(
-    √eps(T),
-    √eps(T),
-    T(eps(T)^(1 / 4)),
-    T(0.95),
-    T(1 / 2),
-    T(2),
-    zero(T),
-    zero(T),
-    I(5),
-    B(true),
-  )
+  function R2ParameterSet(; kwargs...)
+    R2ParameterSet{Float64, Int}(; kwargs...)
+  end
 end
